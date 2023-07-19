8abb4733c8a7ab738bbeef8bd40eb536e2dbaa5c
bcarpenter7 <bennett.carpenter7@gmail.com>
2023-07-19 10:37:17 -0400

    added featured

--- script.js
+++ script.js
@@ -1,44 +1,20 @@
 
-
 const observer = new IntersectionObserver(entries => {
     
     entries.forEach(entry => {
 
         if(entry.isIntersecting){
-            // const projectSection = e.target.querySelector('.hidden')
             entry.target.classList.add('fadeAnimation')
-            // entry.target.classList.remove('hidden')
-            // e.target.classList.remove('hidden')
             return
         }
-        entry.target.classList.remove('fadeAnimation')
     }
     ) 
 })
 
-observer.observe(document.querySelector('.hidden'))
-
-const observerTwo = new IntersectionObserver(entries => {
-    
-    entries.forEach(entry => {
-
-        if(entry.isIntersecting){
-            // const projectSection = e.target.querySelector('.hidden')
-            entry.target.classList.add('fadeAnimation')
-            // entry.target.classList.remove('hidden')
-            // e.target.classList.remove('hidden')
-            return
-        }
-        // entry.target.classList.remove('fadeAnimation')
-    }
-    ) 
-})
-
-observerTwo.observe(document.querySelector('.bioCard'))
-observerTwo.observe(document.querySelector('#cofc'))
-observerTwo.observe(document.querySelector('#arrow1'))
-observerTwo.observe(document.querySelector('#arrow2'))
-observerTwo.observe(document.querySelector('#arrow3'))
-observerTwo.observe(document.querySelector('#teaching'))
-observerTwo.observe(document.querySelector('#generalAssembly'))
-observerTwo.observe(document.querySelector('#imgCard'))
+observer.observe(document.querySelector('.bioCard'))
+observer.observe(document.querySelector('#cofc'))
+observer.observe(document.querySelector('#teaching'))
+observer.observe(document.querySelector('#generalAssembly'))
+observer.observe(document.querySelector('#imgCard'))
+observer.observe(document.querySelector('#featuredDiv'))
+observer.observe(document.querySelector('#projectDiv'))
